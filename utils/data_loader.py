b8103958e2edcd496263394ae204e0beedff24df
Mushfiq5647 <mushfiqfcc6@gmail.com>
2024-10-17 00:55:49 -0500

    Code refactored removing ground truth from model

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,16 +1,9 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
-import torch
-import torchvision.transforms as transforms
-import torch.utils.data as data
-import random
 import json
-from PIL import Image
-from utils.build_vocab import Vocabulary
-from utils.build_vocab import build_vocab
-from utils.build_annotation import Annotation
 import os
-import argparse
+import torch
+import torch.utils.data as data
+from PIL import Image
 
 
 class PoseDataset(data.Dataset):
@@ -24,18 +17,6 @@ class PoseDataset(data.Dataset):
 		self.transform = transform
 		self.seq_length = seq_length
 		self.test_mode = test_mode
-		# self.data = []
-		# self.imroot = os.path.abspath(self.imroot)
-		# self.hroot = os.path.abspath(self.hroot)
-		# self.oproot = os.path.abspath(self.oproot)
-		#
-		#
-		# for sub_dir in self.train_annotation.train_anns:
-		# 	image_dir = os.path.join(self.imroot, sub_dir, 'synchronized','frames')
-		# 	gt_dir = os.path.join(self.imroot, sub_dir, 'synchronized', 'gt-egopose')
-		# 	print("Image Directory",image_dir, gt_dir)
-		# 	self.data.extend(make_dataset(image_dir, gt_dir))
-
 
 	def __getitem__(self, index):
 		imroot = self.imroot
@@ -72,52 +53,6 @@ class PoseDataset(data.Dataset):
 	def __len__(self):
 		return len(self.annotation)
 
-	# def __getitem__(self, index):
-	# 	# Retrieve the sequence of images, ground truth, homography, and OpenPose for the given index
-	# 	image_sequence, gt_sequence, homography_sequence, openpose_sequence = self.data[index]
-	#
-	# 	images = []
-	# 	gts = []
-	# 	homographies = []
-	# 	openposes = []
-	#
-	# 	# Iterate over each frame in the sequence and load the data
-	# 	for image_path, gt_path, homography_path, openpose_path in zip(image_sequence, gt_sequence, homography_sequence,
-	# 																   openpose_sequence):
-	# 		# Load image
-	# 		image = Image.open(image_path)
-	#
-	# 		# Load ground truth
-	# 		with open(gt_path, 'r') as f:
-	# 			gt = f.read()  # Process the ground truth as necessary
-	#
-	# 		# Load homography
-	# 		with open(homography_path, 'r') as f:
-	# 			homography = f.read().split()  # Process homography if needed
-	# 			homography = list(map(float, homography))  # Convert to floats
-	#
-	# 		# Load OpenPose data (assuming JSON format)
-	# 		with open(openpose_path, 'r') as f:
-	# 			openpose = json.load(f)
-	# 			pose = openpose.get('joints', [0] * 48)  # Default to 48 zeros if 'joints' is not present
-	#
-	# 		# Append the loaded data to the lists
-	# 		images.append(image)
-	# 		gts.append(gt)
-	# 		homographies.append(homography)
-	# 		openposes.append(pose)
-	#
-	# 	# Convert lists to tensors
-	# 	images = torch.stack([self.transform(image) if self.transform else image for image in
-	# 						  images])  # Apply transform and stack images
-	# 	gts = torch.Tensor(gts)  # Convert ground truth to tensor
-	# 	homographies = torch.Tensor(homographies)  # Convert homographies to tensor
-	# 	openposes = torch.Tensor(openposes)  # Convert OpenPose data to tensor
-	#
-	# 	# Return the sequence of images, ground truth, homographies, and OpenPose data
-	# 	return images, gts, homographies, openposes
-
-
 def collate_fn(data):
 	""" Creates mini-batch tensors from the list of tuples (images, poses) """
 	data.sort(key=lambda x: len(x[1]), reverse=True)
@@ -125,20 +60,13 @@ def collate_fn(data):
 	images = torch.stack(images, 0)
 	lengths = [len(pose) for pose in target_egoposes]
 	max_length = max(lengths)
-	# Now a 3D tensor to hold [batch_size, sequence_length, 2]
 	targets = torch.zeros(len(target_egoposes), max_length, 75)
-
-	print("Pose2 shape:", len(target_egoposes))
-	print("Max Length:", max_length)
 	for i, pose in enumerate(target_egoposes):
 		end = lengths[i]
 		targets[i, :end, :] = pose[:end]
 	if isinstance(homography[0], torch.Tensor) and isinstance(poses2[0], torch.Tensor):
 		homography = torch.stack(homography, 0)
-		print("Homography stacked to tensor")
 		poses2 = torch.stack(poses2, 0)
-		print("Poses2 stacked to tensor")
-
 	return images, targets, homography, poses2, lengths
 
 def getPair(imroot, hroot, oproot, path, vocab, index, test_mode):
@@ -155,16 +83,14 @@ def getPair(imroot, hroot, oproot, path, vocab, index, test_mode):
 			js = json.loads(f.read())
 			if 'people' not in js or len(js['people']) == 0:
 				# No people detected, handle missing data
-				pose2 = [0] * 75  # Default to 75 zeros (assuming 25 joints * 3 values: x, y, confidence)
+				pose2 = [0] * 75
 			else:
-				# Extract the keypoints for the first person in the 'people' array
 				pose_keypoints = js['people'][0].get('pose_keypoints_2d', [])
 
 				if len(pose_keypoints) == 0:
 					# If no keypoints are found, set to a default value
-					pose2 = [0] * 75  # Default to 75 zeros (25 joints * 3 values: x, y, confidence)
+					pose2 = [0] * 75
 				else:
-					# If keypoints are found, use them
 					pose2 = pose_keypoints
 	else:
 		h = None
@@ -173,39 +99,10 @@ def getPair(imroot, hroot, oproot, path, vocab, index, test_mode):
 	egopose_file = imroot + "/" + path + "/synchronized/gt-egopose/p" + str(index) + ".txt"
 	with open(egopose_file, 'r') as f:
 		egopose_gt = list(map(float, f.read().split()))
-		# print("Egopose:",egopose_file, egopose_gt)
-		# Convert to a list of 75 float values
-	# print("Upper Cluster:", upp_cluster)
-	# print("Lower Cluster:", low_cluster)
+
 	path = path + "/synchronized/frames/imxx" + str(index) + ".jpg"
 	image = Image.open(os.path.join(imroot, path)).convert('RGB')
 	return image, egopose_gt, h, pose2
-
-# def make_dataset(image_dir, gt_dir, h_dir, oproot, seq_length):
-#     data = []
-#     image_dir = os.path.normpath(image_dir)
-#     gt_dir = os.path.normpath(gt_dir)
-#     h_dir = os.path.normpath(h_dir)
-#     oproot = os.path.normpath(oproot)
-#
-#     if os.path.exists(image_dir) and os.path.exists(gt_dir):
-#         images = sorted(os.listdir(image_dir))
-#         gts = sorted(os.listdir(gt_dir))
-#         homographies = sorted(os.listdir(h_dir))
-#         openposes = sorted(os.listdir(oproot))
-#
-#         # Iterate over the images, ground truth, homography, and OpenPose data in steps of seq_length
-#         for i in range(0, len(images) - seq_length + 1, seq_length):
-#             image_sequence = images[i:i + seq_length]
-#             gt_sequence = gts[i:i + seq_length]
-#             homography_sequence = homographies[i:i + seq_length]
-#             openpose_sequence = openposes[i:i + seq_length]
-#
-#             # Store the entire sequence as one entry in the list
-#             data.append((image_sequence, gt_sequence, homography_sequence, openpose_sequence))
-#
-#     return data
-
 
 def get_loader(annotation, imroot, hroot, oproot, vocab, transform, batch_size, shuffle, num_workers, seq_length, test_mode = False):
 	""" Returns torch.utils.data.DataLoader for custom pose dataset. """
